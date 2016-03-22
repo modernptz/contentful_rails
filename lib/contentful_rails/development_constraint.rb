af1fb0f0a738ad3a93023475a1858a9d24ce321d
Yuriy Gatilin <modernptz@gmail.com>
2016-03-22 15:54:52 +0100

    Update development_constraint.rb

--- lib/contentful_rails/development_constraint.rb
+++ lib/contentful_rails/development_constraint.rb
@@ -1,5 +1,5 @@
 class ContentfulRails::DevelopmentConstraint
   def self.matches?(request)
-    Rails.env =~ %r{development} && (request.remote_ip =~ %r{127.0.0} || request.remote_ip =~ %r{^::1$})
+    Rails.env =~ %r{development}
   end
 end
